4fb3244a7747a5ff093410588253b6109337e1ef
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2023-08-19 14:28:52 +0700

    Turns out, it's TCP socket all along

--- plugin.gd
+++ plugin.gd
@@ -4,12 +4,16 @@ extends EditorPlugin
 
 const SETTINGS = {
 	"application/quixel_bridge/socket_port": 24981, # Default export port
+	"application/quixel_bridge/tcp_wait_time": 100, # 100ms
+	"application/quixel_bridge/max_tcp_wait_loops": 10, # 10 loops
 }
 
 var _port: int
 var _server: TCPServer
-var _stream_peer: StreamPeerTCP
-var _socket_peer: WebSocketPeer
+var _stream_peer: StreamPeer
+
+var _wait_time: int
+var _max_wait_loops: int
 
 
 func _enter_tree() -> void:
@@ -21,6 +25,9 @@ func _enter_tree() -> void:
 		ProjectSettings.set_as_basic(k, true)
 	
 	_port = ProjectSettings["application/quixel_bridge/socket_port"]
+	_wait_time = ProjectSettings["application/quixel_bridge/tcp_wait_time"]
+	_max_wait_loops = ProjectSettings["application/quixel_bridge/max_tcp_wait_loops"]
+
 	_start_server()
 
 
@@ -43,13 +50,30 @@ func _start_server() -> void:
 	print("Setting up socket server...")
 	_server = TCPServer.new()
 	_server.listen(_port, "127.0.0.1") # Only allow local connections
-	_stream_peer = _server.take_connection()
-
-	_socket_peer = WebSocketPeer.new()
-	_socket_peer.accept_stream(_stream_peer)
 
 
 func _stop_server() -> void:
 	print("Stopping socket server...")
 	_server.stop()
-	_stream_peer = null
+
+
+func _process(delta: float) -> void:
+	if _server.is_connection_available():
+		print("Received connection from Quixel Bridge... probably.")
+		_stream_peer = _server.take_connection()
+
+		var available := 0
+		var timeout := _max_wait_loops
+		while timeout > 0:
+			if _stream_peer.get_available_bytes() > available:
+				available = _stream_peer.get_available_bytes()
+				timeout = 10
+			else:
+				timeout -= 1
+				OS.delay_msec(_wait_time)
+		
+		print("Done waiting for data.")
+		var text := _stream_peer.get_utf8_string(available)
+
+		print(text)
+
