517346472d3cf003b93967ab408dbba704ab5e9e
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2023-08-19 12:44:23 +0700

    Add plugin settings

--- plugin.gd
+++ plugin.gd
@@ -2,11 +2,26 @@
 extends EditorPlugin
 
 
-func _enter_tree():
-	# Initialization of the plugin goes here.
-	pass
+const SETTINGS = {
+	"application/quixel_bridge/socket_port": 24981, # Default export port
+}
 
 
-func _exit_tree():
-	# Clean-up of the plugin goes here.
-	pass
+func _enter_tree() -> void:
+	for k in SETTINGS:
+		if not ProjectSettings.has_setting(k):
+			ProjectSettings.set_setting(k, SETTINGS[k])
+
+		ProjectSettings.set_initial_value(k, SETTINGS[k])
+		ProjectSettings.set_as_basic(k, true)
+
+
+func _enable_plugin() -> void:
+	push_warning(("BUG: Quixel Bridge settings will not appear until the project settings window is reopened."))
+
+
+func _disable_plugin() -> void:
+	for k in SETTINGS:
+		ProjectSettings.set_setting(k, null)
+	
+	push_warning(("BUG: Quixel Bridge settings will not disappear until the project settings window is reopened."))
